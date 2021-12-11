50f9907b81ffeff6d798de8c2f46a1ba743188cf
rrrx-4 <69399883+rrrx-4@users.noreply.github.com>
2021-12-12 04:00:29 +0530

    12.12.21

---
+++ DS_JAVA/REDO/maxoarr.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+
+        int n  = scn.nextInt();
+        int[] a = new int[n];
+        for(int i=0; i<n; i++)
+        {
+            a[i] = scn.nextInt();
+        }
+        System.out.println(maxOfArray(a,0));
+    }
+
+    public static int maxOfArray(int[] arr, int idx){
+        if(idx>=arr.length);
+        return Integer.MIN_VALUE;
+
+        int r  = maxOfArray(arr,idx+1);
+        if(arr[idx]>r)
+        {
+            return arr[idx];
+        }
+        return r ;
+    }
+
+}
