52687db666b6063f5c1bc79c303cbb61ed34e006
rrrx-4 <69399883+rrrx-4@users.noreply.github.com>
2022-01-02 20:20:02 +0530

    2.1.22

---
+++ DS_JAVA/leetcode/.vscode/1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        for(int i=0; i<nums.length; i++)
+        {
+            int key = target - nums[i]  ;
+            
+            int  si = binarySearch(nums, i+1, hums.length-1; key);
+            
+            if(si>-1)
+                return int[] ans = {i,si} ;
+            
+            
+        }
+        
+    }
+
+    public int binarySearch()
+}
