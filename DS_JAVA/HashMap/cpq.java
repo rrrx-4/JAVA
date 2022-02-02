965e1ec1445ae66f440c0db5ddd501d86e1fc36b
rrrx-4 <69399883+rrrx-4@users.noreply.github.com>
2022-02-02 20:56:51 +0530

    2.2.22

---
+++ DS_JAVA/HashMap/cpq.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+public class cpq {
+
+    public static class PriorityQueue {
+        ArrayList<Integer> data;
+
+        public PriorityQueue() {
+            data = new ArrayList<>();
+        }
+
+        public void add(int val) {
+            // write your code here
+            data.add(val);
+            int ci = size() - 1;
+
+            upheapify(ci);
+
+        }
+
+        void swap(int ci, int pi) {
+            int t = data.get(pi);
+            int t1 = data.get(ci);
+
+            data.set(pi, t1);
+            data.set(ci, t);
+
+        }
+
+        void upheapify(int ci) {
+            if (ci == 0)
+                return;
+
+            int pi = (ci - 1) / 2;
+            if (data.get(ci) < data.get(pi)) {
+                swap(pi, ci);
+                upheapify(pi);
+            }
+        }
+
+        public int remove() {
+            // write your code here
+
+        }
+
+        public int peek() {
+            // write your code here
+            return data.get(0);
+        }
+
+        public int size() {
+            // write your code here
+            return data.size();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue qu = new PriorityQueue();
+
+        String str = br.readLine();
+        while (str.equals("quit") == false) {
+            if (str.startsWith("add")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                qu.add(val);
+            } else if (str.startsWith("remove")) {
+                int val = qu.remove();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("peek")) {
+                int val = qu.peek();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("size")) {
+                System.out.println(qu.size());
+            }
+            str = br.readLine();
+        }
+    }
+}
