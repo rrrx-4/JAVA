42950600feb7c4591def859ad2aa188791d92ce4
rrrx-4 <69399883+rrrx-4@users.noreply.github.com>
2022-02-04 17:11:11 +0530

    4.2.22

--- DS_JAVA/HashMap/cpq.java
+++ DS_JAVA/HashMap/cpq.java
@@ -81,3 +81,71 @@ public class cpq {
         }
     }
 }
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static class PriorityQueue {
+        ArrayList<Integer> data;
+
+        public PriorityQueue() {
+            data = new ArrayList<>();
+        }
+
+        public void add(int val) {
+            // write your code here
+
+        }
+
+    void upheapify(int ci)
+   
+
+    }
+
+    public int remove() {
+      // write your code here
+      swap(0, size()-1);
+
+      int ans = data.remove(size()-1);
+
+      downheapi
+    }
+
+        public int peek() {
+            // write your code here
+            return data.get(0);
+        }
+
+        public int size() {
+            // write your code here
+            return data.size();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue qu = new PriorityQueue();
+
+        String str = br.readLine();
+        while (str.equals("quit") == false) {
+            if (str.startsWith("add")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                qu.add(val);
+            } else if (str.startsWith("remove")) {
+                int val = qu.remove();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("peek")) {
+                int val = qu.peek();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("size")) {
+                System.out.println(qu.size());
+            }
+            str = br.readLine();
+        }
+    }
+}
